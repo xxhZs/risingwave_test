50d5e4a0f57042a9f9f2790266410f75a54b5490
Peng Chen <peng@singularity-data.com>
2022-04-14 18:53:23 +0800

    tmp
    Signed-off-by: Peng Chen <peng@singularity-data.com>

--- src/connector/src/kafka/source/reader.rs
+++ src/connector/src/kafka/source/reader.rs
@@ -18,87 +18,81 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use futures::StreamExt;
 use rdkafka::config::RDKafkaLogLevel;
 use rdkafka::consumer::{DefaultConsumerContext, StreamConsumer};
 use rdkafka::ClientConfig;
+use futures::StreamExt;
 use risingwave_common::error::ErrorCode::{InternalError, ProtocolError};
 use risingwave_common::error::RwError;
+use crate::AnyhowProperties;
 
 use crate::base::{InnerMessage, SourceReader};
 use crate::kafka::split::KafkaSplit;
-use crate::kafka::KAFKA_CONFIG_BROKER_KEY;
+use crate::kafka::{KAFKA_CONFIG_BROKER_KEY, KAFKA_CONFIG_CONSUME_GROUP, KAFKA_CONFIG_SCAN_STARTUP_MODE};
 
 const KAFKA_MAX_FETCH_MESSAGES: usize = 1024;
 
 pub struct KafkaSplitReader {
     consumer: Arc<StreamConsumer<DefaultConsumerContext>>,
-    assigned_splits: HashMap<String, Vec<KafkaSplit>>,
 }
 
 #[async_trait]
 impl SourceReader for KafkaSplitReader {
     async fn next(&mut self) -> Result<Option<Vec<InnerMessage>>> {
+        // todo: use partition queue
         let mut stream = self
             .consumer
             .stream()
             .ready_chunks(KAFKA_MAX_FETCH_MESSAGES);
 
-        let chunk = match stream.next().await {
-            None => return Ok(None),
-            Some(chunk) => chunk,
-        };
-
-        chunk
-            .into_iter()
-            .map(|msg| msg.map_err(|e| anyhow!(e)).map(InnerMessage::from))
-            .collect::<Result<Vec<InnerMessage>>>()
-            .map(Some)
+        stream.next().await.map(|chunk| {
+            chunk
+                .into_iter()
+                .map(|msg| msg.map_err(|e| anyhow!(e)).map(InnerMessage::from))
+                .collect::<Result<Vec<InnerMessage>>>()
+        }).transpose()
     }
 
     async fn new(
         properties: HashMap<String, String>,
         _state: Option<crate::ConnectorState>,
     ) -> Result<Self>
-    where
-        Self: Sized,
+        where
+            Self: Sized,
     {
-        let bootstrap_servers = properties.get(KAFKA_CONFIG_BROKER_KEY).ok_or_else(|| {
-            RwError::from(ProtocolError(format!(
-                "could not found config {}",
-                KAFKA_CONFIG_BROKER_KEY
-            )))
-        })?;
-
+        let properties = AnyhowProperties::new(properties);
+        let broker = properties.get_kafka(KAFKA_CONFIG_BROKER_KEY)?;
         let mut config = ClientConfig::new();
+
+        let auto_offset_reset = match properties.get_kafka(KAFKA_CONFIG_SCAN_STARTUP_MODE).ok() {
+            None => "smallest",
+            Some(config) => match config.as_str() {
+                "earliest" => "smallest",
+                "latest" => "latest",
+                key => return Err(anyhow!("{} config does not support option {}", KAFKA_CONFIG_SCAN_STARTUP_MODE, key)),
+            }
+        };
 
         // disable partition eof
         config.set("enable.partition.eof", "false");
         config.set("enable.auto.commit", "false");
-        config.set("auto.offset.reset", "smallest");
-        config.set("bootstrap.servers", bootstrap_servers);
+        config.set("auto.offset.reset", auto_offset_reset);
+        config.set("bootstrap.servers", broker);
 
-        if config.get("group.id").is_none() {
-            config.set(
-                "group.id",
-                format!(
-                    "consumer-{}",
-                    SystemTime::now()
-                        .duration_since(UNIX_EPOCH)
-                        .unwrap()
-                        .as_micros()
-                ),
-            );
-        }
+        config.set("group.id", properties.get(KAFKA_CONFIG_CONSUME_GROUP).ok().unwrap_or(format!(
+            "consumer-{}",
+            SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_micros()) as String));
 
         let consumer = config
             .set_log_level(RDKafkaLogLevel::Info)
             .create_with_context(DefaultConsumerContext)
-            .map_err(|e| RwError::from(InternalError(format!("consumer creation failed {}", e))))?;
+            .map_err(|e| anyhow!("consumer creation failed {}", e.to_string()))?;
 
         Ok(Self {
             consumer: Arc::new(consumer),
-            assigned_splits: HashMap::new(),
         })
     }
 }
